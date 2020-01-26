417112db2f98499ab7186c335b49114083ce13bc
dstarkey23 <david.starkey@vortexa.com>
2020-01-26 15:50:04 +0000

    begin nlp tests

--- scratch.py
+++ scratch.py
@@ -1,25 +1,70 @@
 import pandas as pd
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn import metrics
 
+class nlp_model:
+
+    def __init__(self):
+        self.train_file = './data/train.csv'
+        self.test_file = './data/test.csv'
+        self.model = Pipeline([
+            ('vect', CountVectorizer()),
+            ('tfidf', TfidfTransformer()),
+            ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                  alpha=1e-3, random_state=42,
+                                  max_iter=5, tol=None)),
+        ])
+
+
+    def load_data(self):
+        self.train = pd.read_csv(self.train_file)
+        self.test  = pd.read_csv(self.test_file)
+
+        '''some exploratory analysis'''
+        # identify common key words
+        keywords = self.test['keyword'].value_counts(dropna=False)
+        print('most common keywords...\n', keywords.head(10))
+        print('\nleast common keywords...\n', keywords.tail(10))
+        print('\n\n\n')
+
+        # identify common locations
+        locations = self.test['location'].value_counts(dropna=False)
+        print('most common locations...\n', locations.head(10))
+        print('\nleast common locations...\n', locations.tail(10))
+        print('\n\n\n')
+
+
+    def gridsearch_model(self,parameters = {'vect__ngram_range': [(1, 1), (1, 2)],
+                                     'tfidf__use_idf': (True, False),
+                                     'clf__alpha': (1e-2, 1e-3)}):
+        '''now try SVM model'''
+        #self.model.fit(self.X_train, self.y_train)
+        #self.predicted = self.model.predict(self.X_test)
+        self.model_gs = GridSearchCV(self.model, parameters, cv=5, n_jobs=-1)
+        self.model_gs = self.model_gs.fit(self.X_train, self.y_train)
+        for param_name in sorted(parameters.keys()):
+            print("%s: %r" % (param_name, self.model_gs.best_params_[param_name]))
+
+
+    def print_performance_metrics(self,predicted,target,names=None):
+        '''
+
+        :return:
+        '''
+        print(metrics.classification_report(target, predicted,target_names=names))
 
 
 if __name__ == '__main__':
-    train = pd.read_csv('./data/train.csv')
-    test = pd.read_csv('./data/test.csv')
+    cl =nlp_model()
+    cl.load_data()
 
 
 
-    '''some exploratory analysis'''
 
-    # identify common key words
-    keywords = test['keyword'].value_counts(dropna=False)
-    print('most common keywords...\n',keywords.head(10))
-    print('\nleast common keywords...\n',keywords.tail(10))
-    print('\n\n\n')
 
-    # identify common locations
-    locations = test['location'].value_counts(dropna=False)
-    print('most common locations...\n', locations.head(10))
-    print('\nleast common locations...\n', locations.tail(10))
-    print('\n\n\n')
+
+
 
 
---
+++ scratch_nlp_example.py
@@ -0,0 +1,120 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+
+categories = ['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med']
+
+
+twenty_train = fetch_20newsgroups(subset='train',
+                                  categories=categories,
+                                  shuffle=True,
+                                  random_state=42)
+'''diagnpstics'''
+print(twenty_train.target_names)
+print(len(twenty_train.data))
+print(len(twenty_train.filenames))
+print("\n".join(twenty_train.data[0].split("\n")[:3]))
+print(twenty_train.target_names[twenty_train.target[0]])
+print(twenty_train.target[:10])
+for t in twenty_train.target[:10]:
+    print(twenty_train.target_names[t])
+
+'''
+Tokenizing text with scikit-learn
+'''
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(twenty_train.data)
+print(X_train_counts.shape)
+count_vect.vocabulary_.get(u'algorithm')
+
+'''from occurences to frequencies'''
+from sklearn.feature_extraction.text import TfidfTransformer
+tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+X_train_tf = tf_transformer.transform(X_train_counts)
+print(X_train_tf.shape)
+
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+print(X_train_tfidf.shape)
+
+
+
+'''
+now train models
+'''
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
+
+'''
+predict on new data
+'''
+docs_new = ['God is love', 'OpenGL on the GPU is fast']
+X_new_counts = count_vect.transform(docs_new)
+X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+predicted = clf.predict(X_new_tfidf)
+for doc, category in zip(docs_new, predicted):
+    print('%r => %s' % (doc, twenty_train.target_names[category]))
+
+
+
+
+'''
+build a pipeline
+'''
+from sklearn.pipeline import Pipeline
+text_clf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultinomialNB()),
+])
+#can now train model with a single command
+text_clf.fit(twenty_train.data, twenty_train.target)
+
+
+
+'''evaluate model performance'''
+import numpy as np
+twenty_test = fetch_20newsgroups(subset='test',
+    categories=categories, shuffle=True, random_state=42)
+docs_test = twenty_test.data
+predicted = text_clf.predict(docs_test)
+np.mean(predicted == twenty_test.target)
+
+
+
+
+'''now try SVM model'''
+from sklearn.linear_model import SGDClassifier
+text_clf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                          alpha=1e-3, random_state=42,
+                          max_iter=5, tol=None)),
+])
+
+text_clf.fit(twenty_train.data, twenty_train.target)
+predicted = text_clf.predict(docs_test)
+np.mean(predicted == twenty_test.target)
+
+
+'''sklearn performance metrics'''
+
+from sklearn import metrics
+print(metrics.classification_report(twenty_test.target, predicted,
+                                    target_names=twenty_test.target_names))
+
+
+
+'''parameter tuning using grid search'''
+from sklearn.model_selection import GridSearchCV
+parameters = {
+     'vect__ngram_range': [(1, 1), (1, 2)],
+     'tfidf__use_idf': (True, False),
+     'clf__alpha': (1e-2, 1e-3),
+ }
+gs_clf = GridSearchCV(text_clf, parameters, cv=5, n_jobs=-1)
+gs_clf = gs_clf.fit(twenty_train.data[:400], twenty_train.target[:400])
+twenty_train.target_names[gs_clf.predict(['God is love'])[0]]
+print(gs_clf.best_score_)
+for param_name in sorted(parameters.keys()):
+    print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
